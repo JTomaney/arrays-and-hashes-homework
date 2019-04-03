2baa17268a6e376761f9ebd8063c5d49b12d4e30
Jonathan Tomaney <jon.tomaney@gmail.com>
2019-04-03 20:11:54 +0100

    wrote and passed second function

--- optional_lab_array_hash_loop/start_point/my_functions.rb
+++ optional_lab_array_hash_loop/start_point/my_functions.rb
@@ -2,3 +2,8 @@ def add_array_lengths(array1, array2)
   total_array_length = array1.length + array2.length
   return total_array_length
 end
+
+def sum_array(numbers)
+  sum_array_result = numbers.sum
+  return sum_array_result
+end
